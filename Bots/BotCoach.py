4e9742af6d696d79fb6128254e004db4867535cf
MurilloGuindani <34457316+MurilloGuindani@users.noreply.github.com>
2021-03-09 10:33:06 -0300

    asd

---
+++ Bots/BotCoach.py
@@ -0,0 +1,28 @@
+from Bots.Bot import Bot
+
+class BotZangado(Bot):
+    def __init__(self,nome):
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self,nome):
+        self.__nome = nome
+
+    def apresentacao(self):
+        return "Ola eu sou o coach quântico"
+ 
+    def mostra_comandos(self):
+        pass
+    
+    def executa_comando(self,cmd):
+        pass
+
+    def boas_vindas(self):
+        pass
+
+    def despedida(self):
+        pass
