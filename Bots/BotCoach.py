3117c1c785df1d4bc0a9156e33254fc0a66c5028
MurilloGuindani <34457316+MurilloGuindani@users.noreply.github.com>
2021-03-09 10:50:16 -0300

    boat coach quase pronto

--- Bots/BotCoach.py
+++ Bots/BotCoach.py
@@ -1,7 +1,8 @@
 from Bots.Bot import Bot
 
 class BotZangado(Bot):
-    def __init__(self,nome):
+    def __init__(self, comandos):        
+        super().__init__(comandos)
         self.__nome = nome
 
     @property
@@ -13,16 +14,19 @@ class BotZangado(Bot):
         self.__nome = nome
 
     def apresentacao(self):
-        return "Ola eu sou o coach quântico"
+        return "Eu sou o BotCoach e estou aqui para despertar o seu potencial evolutivo e girar a chave da sua vida"
  
     def mostra_comandos(self):
-        pass
+        c = 0
+        for key in self.__comandos:
+            c += 1
+            return str(c) + " - " + key
     
     def executa_comando(self,cmd):
-        pass
+        return self.__comandos[cmd]
 
     def boas_vindas(self):
-        pass
+        return "Oi, quer mudar a sua vibraçao? Eu posso mudar o seu DNA quantico da quinta dimensao por apenas R$ 40.000,00"
 
     def despedida(self):
-        pass
+        return "Adeus, pena que não consegui mudar seu mindset ;////"
